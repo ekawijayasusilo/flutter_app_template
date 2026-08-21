701530f0e93bd6f9a28b76e3f4d9f515bce921a2
Eka Wijaya Susilo <ekawijayasusilo@gmail.com>
2026-08-21 23:32:18 +0700

    initialize design system package via very_good_cli

---
+++ packages/design_system/lib/src/extensions/build_context_extensions.dart
@@ -0,0 +1,13 @@
+import 'package:design_system/design_system.dart';
+
+/// Extension on [BuildContext] for easy access to custom theme tokens.
+extension AppThemeBuildContext on BuildContext {
+  /// Returns the [AppColors] from the current theme.
+  AppColors get appColors => Theme.of(this).extension<AppColors>()!;
+
+  /// Returns the [AppSpacing] from the current theme.
+  AppSpacing get appSpacing => Theme.of(this).extension<AppSpacing>()!;
+
+  /// Returns the [AppTextStyles] from the current theme.
+  AppTextStyles get appTextStyles => Theme.of(this).extension<AppTextStyles>()!;
+}
