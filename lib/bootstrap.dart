63a032e6ac708ccc16a8271c5e9a23e2ccf4cde3
Eka Wijaya Susilo <ekawijayasusilo@gmail.com>
2026-08-24 12:59:53 +0700

    feat: initialize firebase integration and ai skills (#7)

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -20,14 +21,21 @@ class AppBlocObserver extends BlocObserver {
   }
 }
 
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+Future<void> bootstrap({
+  required FirebaseOptions firebaseOptions,
+  required FutureOr<Widget> Function() builder,
+}) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   Bloc.observer = const AppBlocObserver();
 
-  // Add cross-flavor configuration here
+  await Firebase.initializeApp(
+    options: firebaseOptions,
+  );
 
   runApp(await builder());
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_app_template/app/app.dart';
 import 'package:flutter_app_template/bootstrap.dart';
+import 'package:flutter_app_template/firebase_options.dart';
 
 Future<void> main() async {
-  await bootstrap(() => const App());
+  await bootstrap(
+    firebaseOptions: DefaultFirebaseOptions.currentPlatform,
+    builder: () => const App(),
+  );
 }
